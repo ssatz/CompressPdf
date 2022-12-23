1ce8395df009b3710229671784c598795a36857e
sathish <sathish.thi@gmail.com>
2022-12-23 12:07:37 +0530

    Initial commit

---
+++ server.mjs
@@ -0,0 +1,74 @@
+'use strict';
+import express from 'express';
+import { S3Client, GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
+import * as fs from 'fs'
+import morgan from 'morgan'
+const s3Client = new S3Client({ region: process.env.AWS_S3_REGION });
+import {execSync} from 'child_process';
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+morgan('combined');
+app.use(express.json());
+app.use(
+  fileUpload()
+);
+app.post('/compress', (req, res) => {
+  handler(req.body.filename)
+  return res.json({requestBody: req.body})
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
+
+
+export const handler = async (filename) => {
+  if(filename==null || typeof filename == undefined ){
+    console.log('Empty filename');
+    return;
+  }
+  
+  const streamToString = (stream) =>
+    new Promise((resolve, reject) => {
+      const chunks = [];
+      stream.on("data", (chunk) => chunks.push(chunk));
+      stream.on("error", reject);
+      stream.on("end", () => resolve(Buffer.concat(chunks)));
+    });
+
+  // Get the object from the Amazon S3 bucket. It is returned as a ReadableStream.
+  const data = await s3Client.send(new GetObjectCommand({
+    Bucket: process.env.AWS_S3_BUCKET,
+    Key: filename,
+     contentType : 'application/pdf'
+  }));
+  // Convert the ReadableStream to a string.
+  const bodyContents = await streamToString(data.Body);
+  fs.writeFileSync(`/tmp/${filename}`, bodyContents);
+  const outputFilename = `compressed_${filename}`;
+  
+  console.log(outputFilename);
+  
+  const cmd = `gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=/tmp/${outputFilename} /tmp/${filename}`;
+  console.log(cmd);
+  let logs = execSync(cmd,{stdio: 'inherit'});
+  console.log(logs);
+
+  if (fs.existsSync(`/tmp/${outputFilename}`)) {
+   const fileStream = fs.createReadStream(`/tmp/${outputFilename}`);
+    
+    await s3Client.send(new PutObjectCommand({
+      Bucket: process.env.AWS_S3_BUCKET,
+      Key: outputFilename,
+      Body: fileStream,
+      contentType : 'application/pdf'
+    }));
+    execSync(`rm /tmp/${outputFilename}`);
+    console.log('ends here');
+  }
+};
